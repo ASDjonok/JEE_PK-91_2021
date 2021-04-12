17a775f53af35738504cf0fb29c275f53f172d4b
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2021-04-12 10:07:49 +0300

    update front-end for library functionality

---
+++ src/main/webapp/my_script.js
@@ -0,0 +1,90 @@
+;
+
+function onAddToLibraryButtonClick() {
+    var data = document.getElementsByTagName('input')[0].value;
+    var data1 = document.getElementsByTagName('input')[1].value;
+    var data2 = document.getElementsByTagName('input')[2].value;
+    console.log(data);
+    console.log(data1);
+    console.log(data2);
+
+    /*var xhr = new XMLHttpRequest();
+    var body = 'name=' + encodeURIComponent(data) +
+        '&surname=' + encodeURIComponent("Aleshchenko2");
+    xhr.open("POST", 'http://localhost:8080/library', true);
+    xhr.send(body);*/
+}
+
+function onLibraryButtonClick() {
+// GET
+    fetch('http://localhost:8080/library')
+        .then(response => {
+            // console.log("2222")
+            async function f1() {
+                var responseText = await response.text();
+                console.log("Response = " + responseText); // 10
+                document.getElementsByTagName("textarea")[0].value = responseText;
+            }
+            f1();
+        });
+}
+
+
+
+
+/*for (let i = 0; i < 10000; i++) {
+    console.log("11111")
+}*/
+//POST
+/*var xhr = new XMLHttpRequest();
+
+var body = 'name=' + encodeURIComponent("Oleksii2") +
+    '&surname=' + encodeURIComponent("Aleshchenko2");
+
+xhr.open("POST", 'http://localhost:8080/hello-world', true);
+// xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+/!*xhr.onreadystatechange =
+...
+;*!/
+
+xhr.send(body);*/
+
+/*console.log("in JavaScript");
+
+alert("AAAAAAAAAAAAAA!!!!!");
+
+let age = prompt('Сколько тебе лет?', 100);
+
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!*/
+
+/*fetch('http://localhost:8080/hello-world')
+    .then(response => {
+        // console.log(response/!*.json()*!/)
+
+        async function f1() {
+            var x = await response.text();
+            console.log("Response = " + x); // 10
+        }
+
+        f1();
+
+        // console.log(await response.text()/!*.json()*!/)
+        // console.log(response.json())
+    })*/
+// .then(commits => alert(commits[0].author.login));
+
+/*
+let response = await fetch("http://localhost:8080/hello-world");
+
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+                   // получаем тело ответа (см. про этот метод ниже)
+
+    let json = await response.json();
+    console.log(json);
+    alert(json);
+} else {
+    alert("Ошибка HTTP: " + response.status);
+}*/
+
+
---
+++ src/main/webapp/old2_index.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASDjonok
+  Date: 22.02.2021
+  Time: 11:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title3</title>
+    <link href="test.css" rel="stylesheet" type="text/css">
+    <script src="new_myscripts.js"></script>
+<%--    <script src="new_myscripts2.js"></script>--%>
+</head>
+<body>
+
+    <input type="text" size="40">
+
+    <button onclick="onButtonClick()">MyButton</button>
+
+<%--<%= "Hello World" + 1/2%>
+
+<%System.out.println("!!!!!!!!!!!ASD");%>
+<%out.println("<p>!!!!!!!!!!!ASD</p>");%>
+<%="<p>Test</p>"%>--%>
+
+<%--<form action="/submit" method="GET">
+    <input name="name" value="Ivan">
+    <input name="surname" value="Ivanov">
+</form>--%>
+
+</body>
+</html>
